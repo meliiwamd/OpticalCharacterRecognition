90c52e07cf8bb1624a2311f7072002cbdd2e7eb0
Melika Ahmadi Ranjbar <melika.ahmadi2000@yahoo.com>
2022-01-16 09:05:54 +0330

    Create Directories

---
+++ Implementation/DataCollection.py
@@ -0,0 +1,11 @@
+# This File Is For The First Phase
+# Data Collection
+
+# We Use Two Methods -> 1) Artificial Dataset 2) Actual Dataset
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
